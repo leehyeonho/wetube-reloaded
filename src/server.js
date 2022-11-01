0cdf8005e98ee5f38ae375334f5c42f58c29757a
leehyeonho <dlgusgh0812@gmail.com>
2022-11-01 11:47:47 +0900

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -4,10 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go hone.");
-
+const handleHome = (req, res) => {
+    return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+};
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}.`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
