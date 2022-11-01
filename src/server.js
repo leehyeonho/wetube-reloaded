24212f617adb7f00447d86cc89254cc7ef2a9b50
leehyeonho <dlgusgh0812@gmail.com>
2022-11-01 10:45:57 +0900

    #3.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go hone.");
+
+app.get("/", handleHome);
+
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}.`);
 
 app.listen(4000, handleListening);
