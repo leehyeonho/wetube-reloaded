ca572015ddeb48448f89126a9fe656cc6368d4c5
leehyeonho <dlgusgh0812@gmail.com>
2022-11-02 17:03:48 +0900

    #3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,19 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
-const  gossipMiddleware = (req, rse, next) => {
+const  logger = (req, rse, next) => {
     console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
     next();
 }
 
 const handleHome = (req, res) => {
     return res.send("I still love you.");
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-};
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}.`);
 
