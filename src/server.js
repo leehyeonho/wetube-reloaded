6aa1c241fac8eaf9e5b3be32bdaa8bfb0d35d1eb
leehyeonho <dlgusgh0812@gmail.com>
2022-11-02 16:23:53 +0900

    #3,5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,13 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const  gossipMiddleware = (req, rse, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
     return res.send("I still love you.");
 };
 const handleLogin = (req, res) => {
     return res.send("Login here.");
 };
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}.`);
